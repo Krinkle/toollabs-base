db42c794b7d91ad0d0a72c94d86181117b6733d0
Timo Tijhof <krinklemail@gmail.com>
2014-01-17 18:19:23 +0100

    Create sample-toollabs-LocalConfig.php

---
+++ sample-toollabs-LocalConfig.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copy this file and rename it to LocalConfig.php,
+ * then modify that file if you need to.
+ */
+
+// Localization
+define( 'KR_TSINT_START_INC', '/data/project/intuition/src/Intuition/ToolStart.php' );
+
+$kgConf->remoteBase = '//tools.wmflabs.org/example/basetool';

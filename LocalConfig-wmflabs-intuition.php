1b3585c2a8c71f6d8e7059d3acaae0d7ae4dbbe7
Timo Tijhof <krinklemail@gmail.com>
2014-01-17 18:17:19 +0100

    Update LocalConfig-wmflabs-intuition.php

--- LocalConfig-wmflabs-intuition.php
+++ LocalConfig-wmflabs-intuition.php
@@ -3,6 +3,6 @@
  * LocalConfig for BaseTool install in Tool Labs.
  */
 
-define( 'KR_TSINT_START_INC', '/home/project/i/n/t/intuition/ToolserverI18N/ToolStart.php' );
+define( 'KR_TSINT_START_INC', '/data/project/intuition/src/Intuition/ToolStart.php' );
 
 $kgConf->remoteBase = '//tools.wmflabs.org/intuition/basetool';

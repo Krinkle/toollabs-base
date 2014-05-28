3a1138f8be99ce37fe1a6e02cdc4dab35d9942df
Timo Tijhof <krinklemail@gmail.com>
2014-05-28 16:25:04 +0200

    Restructure code base (create src/ and lib/)

--- InitTool.php
+++ InitTool.php
@@ -10,8 +10,8 @@
  * @package toollabs-base
  */
 
-require_once __DIR__ . '/GlobalDefinitions.php';
-require_once __DIR__ . '/GlobalConfig.php';
+require_once __DIR__ . '/src/GlobalDefinitions.php';
+require_once __DIR__ . '/src/GlobalConfig.php';
 
 // Never overwrite $kgConfig, but if not set already
 // make sure GlobalConfig is initiated
@@ -19,33 +19,33 @@ if ( !isset( $kgConf ) || !is_object( $kgConf ) ) {
 	$kgConf = new GlobalConfig();
 }
 
-require_once __DIR__ . '/Request.php';
+require_once __DIR__ . '/src/Request.php';
 
 // POST overrides GET data
 // We don't use $_REQUEST here to avoid interference from cookies.
 $kgReq = new Request( $_POST + $_GET );
 
 function kfIncludeMwClasses() {
-	require_once __DIR__ . '/mw/mock.php';
+	require_once __DIR__ . '/lib/mw/mock.php';
 
 	// Patched to remove:
 	// - Html::htmlHeader()
-	require_once __DIR__ . '/mw/Html.php';
+	require_once __DIR__ . '/lib/mw/Html.php';
 
-	require_once __DIR__ . '/mw/GitInfo.php';
+	require_once __DIR__ . '/lib/mw/GitInfo.php';
 
 	// Patched to remove:
 	// - Sanitizer::decodeCharReferencesAndNormalize ($wgContLang)
 	// - Sanitizer::stripAllTags (StringUtils)
 	// Patches to change:
 	// - Sanitizer::validateEmail (wfRunHooks)
-	require_once __DIR__ . '/mw/Sanitizer.php';
+	require_once __DIR__ . '/lib/mw/Sanitizer.php';
 }
 kfIncludeMwClasses();
 
-require_once __DIR__ . '/HtmlSelect.php';
-require_once __DIR__ . '/GlobalFunctions.php';
-require_once __DIR__ . '/LabsDB.php';
+require_once __DIR__ . '/src/HtmlSelect.php';
+require_once __DIR__ . '/src/GlobalFunctions.php';
+require_once __DIR__ . '/src/LabsDB.php';
 
 // Must be after GlobalFunctions
 $kgConf->initConfig();
@@ -61,5 +61,5 @@ if ( file_exists(  __DIR__ . '/LocalConfig.php' ) ) {
 	require_once __DIR__ . '/LocalConfig.php';
 }
 
-require_once __DIR__ . '/BaseTool.php';
-require_once __DIR__ . '/KrToolBaseClass.php';
+require_once __DIR__ . '/src/BaseTool.php';
+require_once __DIR__ . '/src/KrToolBaseClass.php';

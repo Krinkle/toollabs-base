843dc9723c9b2dfc44af16fa7ed0b036a7a9e0b7
Timo Tijhof <krinklemail@gmail.com>
2014-07-10 05:32:10 +0200

    Remove obsolete LocalConfig-toolserver.php template

--- LocalConfig-toolserver.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * LocalConfig for BaseTool install on Toolserver.
- */
-
-define( 'KR_TSINT_START_INC', '/home/project/i/n/t/intuition/ToolserverI18N/ToolStart.php' );
-
-$kgConf->remoteBase = '//toolserver.org/~krinkle';
